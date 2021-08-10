f12d836f2cb4c077e3493b7e8c08dd3c7bf93af0
atobajas <atobajas@novadiet.es>
2021-08-10 14:54:52 +0200

    Add CORS

--- Modulo4_Nodejs/src/index.ts
+++ Modulo4_Nodejs/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 
 const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    methods: 'GET',
+    origin: 'http://localhost:8080',
+    credentials: true,
+  })
+);
 
 app.use('/', express.static(path.resolve(__dirname, '../public')));
 
