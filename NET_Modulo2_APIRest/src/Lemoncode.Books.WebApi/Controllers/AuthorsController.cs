fc9f3351944f551ce0f0a48dfbbe9bf7b0b3f663
atobajas <atobajas@novadiet.es>
2021-11-15 13:01:54 +0100

    Adding Controllers

---
+++ NET_Modulo2_APIRest/src/Lemoncode.Books.WebApi/Controllers/AuthorsController.cs
@@ -0,0 +1,53 @@
+﻿using Lemoncode.Books.Application;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Lemoncode.Books.WebApi.Controllers
+{    
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorsRepository _authorsRepository;
+
+        // GET: api/<AuthoresController>
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authorsRepository = _authorsRepository.GetAuthors();
+            return Ok(authorsRepository);
+        }
+
+        // GET api/<AuthoresController>/5
+        /// <param name="id" example="00000000-0000-0000-0000-000000000000">The game id</param>
+        /// <param name="isDetailed" example="true">Flag that indicates whether the report is detailed or not</param>
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id)
+        {
+            var authorRepository = _authorsRepository.GetAuthor(id);
+            return Ok(authorRepository);
+        }
+
+        // POST api/<AuthoresController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AuthoresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AuthoresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NET_Modulo2_APIRest/src/Lemoncode.Books.WebApi/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Lemoncode.Books.Application;
+using Lemoncode.Books.Infra.Repository.EfCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lemoncode.Books.WebApi.Extensions
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddSoccerDependencies(this IServiceCollection services)
+        {
+            //services.AddSingleton<IDateTimeService, DateTimeService>();
+            //services.AddTransient<GamesCommandService>();
+            //services.AddTransient<GamesQueryService>();
+            //services.AddSingleton<GameToGameReportMapper>();
+
+            // Registro del repositorio a utilizar
+            //services.AddSingleton<IGamesRepository, InMemoryGamesRepository>();
+            services.AddTransient<IAuthorsRepository, EfCoreAuthorsRepository>();
+
+            return services;
+        }
+    }
+}
