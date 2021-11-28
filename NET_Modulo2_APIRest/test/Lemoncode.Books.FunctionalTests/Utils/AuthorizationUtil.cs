e35722b6c2b2b092b1b96717eb3c4f2ecbe80576
atobajas <atobajas@gmail.com>
2021-11-28 18:09:42 +0100

    Adding Test

---
+++ NET_Modulo2_APIRest/test/Lemoncode.Books.FunctionalTests/Utils/AuthorizationUtil.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lemoncode.Books.FunctionalTests.Utils
+{
+    public static class AuthorizationUtil
+    {
+        public static AuthenticationHeaderValue GetBasicAuthorizationHeaderValue(IConfiguration configuration)
+        {
+            var testUsername = configuration.GetValue<string>("BasicAuthentication:Username");
+            var testPassword = configuration.GetValue<string>("BasicAuthentication:Password");
+            var credentials = $"{testUsername}:{testPassword}";
+            var credentialsBytes = Encoding.UTF8.GetBytes(credentials);
+            var credentialsBase64 = Convert.ToBase64String(credentialsBytes);
+            var result = new AuthenticationHeaderValue("Basic", credentialsBase64);
+            return result;
+        }
+    }
+}
